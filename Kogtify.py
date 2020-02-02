fc433d41d4405df2e5875eac8b3280388e78e8a6
Dmitriy Mishutin <dmitriy.mishutin@cxense.com>
2020-02-02 13:33:14 +0400

    First version

---
+++ Kogtify.py
@@ -0,0 +1,133 @@
+import telebot
+
+import database
+
+import consts
+
+from database import *
+from telebot import types
+
+USER_SUBSCIRBED = 1
+USER_UNSUBSCRIBED = 0
+
+bot = telebot.TeleBot('1022712943:AAH-svQMnwb-J94mtcqydxnNsm786L3x0Wk')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start":
+        ask_nickname(message)
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id,
+                         '''/start - (пере) регистрация
+                            /menu - показать меню
+                         ''')
+    elif message.text == "/menu":
+        send_menu(message)
+
+
+def ask_nickname(message):
+    bot.send_message(message.from_user.id,
+                     "Привет, давай я тебя запишу. Какой у тебя никнейм? (Давай без хуйни только ок?)")
+    create_user(message.from_user.id)
+    bot.register_next_step_handler(message, get_user_nickname)
+
+
+def get_user_nickname(message):
+    nickname = message.text
+    update_user(message.from_user.id, 'real_name', message.text)
+    # ask_for_phone(message)
+    ask_for_subscription(message)
+
+
+# def ask_for_phone(message):
+#    keyboard = types.InlineKeyboardMarkup()
+#    key_add = types.InlineKeyboardButton(
+#        text="Хочу", callback_data='add_number')
+#    key_skip = types.InlineKeyboardButton(
+#        text='Не хочу', callback_data='skip_number')
+#    keyboard.add(key_skip)
+#
+#    question = 'Если хочешь, укажи номер телефона'
+#    bot.send_message(message.from_user.id, text=question,
+#                     reply_markup=keyboard)
+
+
+def get_subscribe(message):
+    phone = message.text
+    update_user(message.from_user.id, 'phone', message.text)
+    ask_for_subscription(message)
+
+
+def ask_for_subscription(message):
+    keyboard = types.InlineKeyboardMarkup()
+    key_accept = types.InlineKeyboardButton(
+        text='Подписаться', callback_data='subscribe'
+    )
+    keyboard.add(key_accept)
+    key_skip = types.InlineKeyboardButton(
+        text='Не хочу', callback_data='unsubscribe')
+    keyboard.add(key_skip)
+
+    question = 'Подписаться на уведомления?'
+    bot.send_message(message.from_user.id, text=question,
+                     reply_markup=keyboard)
+
+
+def send_menu(message):
+    user = get_user(message.from_user.id)
+
+    keyboard = types.InlineKeyboardMarkup()
+    key_azur = types.InlineKeyboardButton(
+        text='!!! Азурегос !!!', callback_data='Азурегос'
+    )
+    keyboard.add(key_azur)
+    key_kazzak = types.InlineKeyboardButton(
+        text='!!! Каззак !!!', callback_data='Каззак')
+    keyboard.add(key_kazzak)
+
+    if user.subscribed == consts.DB_USER_SUBSCRIBED:
+        unsubscribe_btn = types.InlineKeyboardButton(
+            text="Отписаться", callback_data="unsubscribe")
+        keyboard.add(unsubscribe_btn)
+    else:
+        subscribe_btn = types.InlineKeyboardButton(
+            text="Подписаться", callback_data="subscribe")
+        keyboard.add(subscribe_btn)
+
+    question = 'Меню:'
+    bot.send_message(message.from_user.id, text=question,
+                     reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    # if call.data == "skip_number":
+    #    ask_for_subscription(call)
+    # elif call.data == "add_number":
+    #    bot.send_message(call.from_user.id,"Введите номер в формате (8 123 456 78 90)")
+    #    bot.register_next_step_handler(message, get_subscribe)
+    if call.data == "subscribe":
+        set_subscription(call.from_user.id, consts.DB_USER_SUBSCRIBED)
+        bot.send_message(call.from_user.id,
+                         "Я тебя запомнил, уебок")
+        send_menu(call)
+    elif call.data == "unsubscribe":
+        set_subscription(call.from_user.id, consts.DB_USER_UNSUBSCRIBED)
+        bot.send_message(call.from_user.id,
+                         "Я тебя запомнил, уебок")
+        send_menu(call)
+    elif call.data == 'Азурегос' or call.data == 'Каззак':
+        notify_all(call.data, call.from_user.id)
+
+
+def notify_all(boss, by):
+    user = get_user(by)
+    subscribed = get_subscribed_users()
+
+    for subscriber in subscribed:
+        bot.send_message(int(subscriber.name), '''%s обнаружил %s''' %
+                         (user.real_name, boss))
+
+
+bot.polling(none_stop=True, interval=0)
---
+++ database.py
@@ -0,0 +1,85 @@
+
+import sqlite3
+
+import consts
+
+from threading import RLock
+
+
+dbConnection = sqlite3.connect('users.db', check_same_thread=False)
+
+c = dbConnection.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (name text PRIMARY KEY,real_name text ,phone text, subscribed int)''')
+
+dbConnection.commit()
+
+
+dbLock = RLock()
+
+
+class User:
+    def __init__(self, tpl):
+        super().__init__()
+
+        self.name = tpl[0]
+        self.real_name = tpl[1]
+        self.phone = tpl[2]
+        self.subscribed = tpl[3]
+
+
+def create_user(name):
+    with dbLock:
+        c.execute('''DELETE FROM users WHERE name="%s"''' % (name))
+        c.execute(
+            '''INSERT INTO users (name) VALUES ("%s")''' % (name))
+        dbConnection.commit()
+
+
+def update_user(name, field, value):
+
+    with dbLock:
+        c.execute(
+            '''UPDATE users SET %s="%s" WHERE name="%s"''' % (
+                field, value, name)
+        )
+        dbConnection.commit()
+
+
+def set_subscription(name, isSubscribed):
+    with dbLock:
+        c.execute(
+            '''UPDATE users set subscribed=%d WHERE name="%s"''' % (
+                isSubscribed, name)
+        )
+        dbConnection.commit()
+
+
+def get_subscribed_users():
+    with dbLock:
+        c.execute(
+            '''SELECT * FROM users WHERE subscribed=%d''' % (
+                consts.DB_USER_SUBSCRIBED)
+        )
+        rows = c.fetchall()
+        res = list()
+
+        for row in rows:
+            res.append(User(row))
+        return res
+
+
+def get_user(name):
+    with dbLock:
+        c.execute('''SELECT * FROM users WHERE name="%s"''' % (name))
+        user = c.fetchone()
+
+        return User(user)
+
+
+def remove_user(name):
+    with dbLock:
+        c.execute(
+            '''DELETE FROM users WHERE name="%s"''' % (name)
+        )
+        dbConnection.commit()
