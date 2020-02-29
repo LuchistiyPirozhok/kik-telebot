1058711a753f69f754054c56bb854a6f473562f4
Shnuroker <strelok_samara@mail.ru>
2020-02-29 16:32:20 +0400

    Refactor constants

--- constants.py
+++ constants.py
@@ -1,2 +1,10 @@
-DB_USER_SUBSCRIBED = 1
-DB_USER_UNSUBSCRIBED = 0
+
+class Database:
+    DB_USER_SUBSCRIBED = 1
+    DB_USER_UNSUBSCRIBED = 0
+
+
+class Commands:
+    START = "/start"
+    HELP = "/help"
+    MENU = "/menu"
