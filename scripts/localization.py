214bb6600f22113ff94110669be53979999fcc9f
Shnurker <strelok-samara@mail.ru>
2020-03-07 23:09:43 +0300

    Admin menu localization

--- scripts/localization.py
+++ scripts/localization.py
@@ -4,15 +4,16 @@ class Locale:
     BOT_HELP_MESSAGE = '''/start - регистрация
 /menu - показать меню
                         '''
-    BOT_MENU_MESSAGE = '''Меню:'''
+    BOT_MENU_MESSAGE = '''Меню Kik-Notify:'''
+    BOT_ADMIN_MENU = '''Административная часть'''
     CHARACTER_SUBCRIPTION = '''Подписаться'''
     CHARACTER_UNSUBCRIPTION = '''Отписаться'''
     CHARACTER_GUILD = '''В какой гильдии вы состоите?'''
     CHARACTER_QUESTION_ABOUT_SUBCRIPTION = '''Вы хотите подписаться на уведомления?'''
     CHARACTER_SUCCESSFUL_SUBCRIPTION = '''Вы подписались на уведомления'''
     CHARACTER_SUCCESSFUL_UNSUBCRIPTION = '''Вы отписались от уведомлений'''
-    CONFIRM = 'Подтвердить'
-    NOT_CONFIRM = 'Не подтверждать'
+    CONFIRM = 'Зарегистрировать пользователя'
+    NOT_CONFIRM = 'Забанить пользователя'
     DELETE = 'Удалить пользователя из БД'
     REGISTER_FIRST = 'Сначала необходимо завершить реигстрацию'
     CHARACTER_REG_SUCCESSFUL = '''Регистрация успешно подтверждена'''
@@ -36,6 +37,34 @@ class Locale:
     def REGISTRATION_COMPLETE_CONFIRM(user_name, reg_code):
         return f'Поступил запрос регистрации от пользователя {user_name} со следующим кодом {reg_code}. Подтвердить?'
 
+class Admin:
+    ALL_USERS = 'Вывести таблицу со всеми пользователями'
+    ALL_GUILDS = 'Вывести таблицу со всеми гильдиями'
+    ALL_PENDUNG_USERS = 'Вывести список ожидающих подтверждения регистрации пользователей'
+    ALL_BANNED_USERS = 'Вывести список забаненных пользователей'
+    CHANGE_USER_STATUS = 'Изменить статус пользователя по ID'
+    DELETE_USER = 'Удалить пользователя из БД по ID'
+    ALL_ADMINS = 'Вывести список администраторов'
+    MESSAGE_TO_ALL = 'Отправить сообщение всем пользователем'
+    ADD_GUILD = 'Добавить гильдию'
+    USERS_EMPTY = 'Такие пользователи отсутствуют'
+    SELECT_USER_STATUS_BY_ID = 'Введите ID пользователя:'
+    SELECT_USER_STATUS_BY_ID_FAILED = 'Пользователь с таким ID не найден'
+    SELECT_USER_STATUS_BY_ID_SUCCESSFUL = 'Пользователь с таким ID найден'
+    CHANGE_USER_STATUS_BY_ID = '''Введите новый статус пользователя:
+"-1" - забанить
+"2" - активировать
+                        '''
+    CHANGE_USER_STATUS_BY_ID_SUCCESSFUL = 'Статус пользователя изменен'        
+    CHANGE_USER_STATUS_BY_ID_ADD_ADMIN = 'Вам были выданы права администратора'  
+    ADD_ADMIN = 'Добавить админа'
+    NO_PERMITTIONS = 'У вас отсутствуют права на выполнение этого действия'
+    MESSAGE_TEXT = 'Введите сообщение, которое будет отправлено всем активным пользователям с включенной подпиской'
+    MESSAGE_TO_ALL_TITLE = 'Сообщение всем подписчикам'
+
+    @staticmethod
+    def MESSAGE_TO_ALL(user_name, message_to_all):
+        return f'Администратор {user_name} сообщает: "{message_to_all}"'
 
 class LocaleExceptions:
     ENVIRONMENT_API_KEY_NOT_FOUND = '''Environment variable KIK_TELEBOT_API_KEY aren't present'''
@@ -62,3 +91,13 @@ class Messages:
     CONFIRM = 'confirm'
     NOT_CONFIRM = 'not_confirm'
     DELETE = 'delete'
+    ADMIN = 'admin'
+    ALL_USERS = 'all_users'
+    ALL_GUILDS = 'all_guilds'
+    ALL_PENDUNG_USERS = 'all_pending_users'
+    ALL_BANNED_USERS = 'all_banned_users'
+    CHANGE_USER_STATUS = 'change_user_status'
+    DELETE_USER = 'delete_user'
+    ALL_ADMINS = 'all_admins'
+    MESSAGE_TO_ALL = 'message_to_all'
+    ADD_ADMIN = 'add_admin'
